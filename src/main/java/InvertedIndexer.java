9bbf0d00b6e9d2491e514eb95dbd9736287205a9
eillon <1377437556@qq.com>
2018-05-01 12:04:45 +0800

    add some comments

--- src/main/java/InvertedIndexer.java
+++ src/main/java/InvertedIndexer.java
@@ -14,7 +14,6 @@ import org.apache.hadoop.mapreduce.lib.partition.HashPartitioner;
 import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -24,20 +23,24 @@ public class InvertedIndexer {
         @Override
         public void map(Object key, Text value, Context context) throws IOException,InterruptedException{
             FileSplit fileSplit = (FileSplit) context.getInputSplit();
-            String filename = new String(fileSplit.getPath().getName());
-            filename=filename.substring(0,filename.length()-14);
+            String filename = fileSplit.getPath().getName();
+            filename=filename.substring(0,filename.length()-14);          // 去除后缀
 
             Text word = new Text();
             StringTokenizer itr = new StringTokenizer(value.toString());
 
             while (itr.hasMoreTokens()){
-                word.set(itr.nextToken()+"#"+filename);
+                word.set(itr.nextToken()+"#"+filename);                   // 使用 # 将word 与 filename 分开
                 context.write(word,new IntWritable(1));
             }
         }
     }
 
+    /*
+        将key值相同的项合并，减少网络开销
+     */
     public static class SumCombiner extends Reducer<Text,IntWritable,Text,IntWritable>{
+        @Override
         public void reduce(Text key, Iterable<IntWritable> values,Context context) throws IOException, InterruptedException{
             int sum = 0;
             for (IntWritable val : values){
@@ -47,32 +50,31 @@ public class InvertedIndexer {
         }
     }
 
-    public static class MyPartitioner extends HashPartitioner<Text, IntWritable> {
+    /*
+        以 word 为关键字进行划分
+     */
+    public static class InvertedIndexerPartitioner extends HashPartitioner<Text, IntWritable> {
+        @Override
         public int getPartition(Text key, IntWritable value, int numPartitions){
-            String term = new String();
-            term = key.toString().split("#")[0];
-            return super.getPartition(new Text(term),value,numPartitions);
+            String word = key.toString().split("#")[0];
+            return super.getPartition(new Text(word),value,numPartitions);
         }
 
     }
 
-
     public static class InvertedIndexerReducer extends Reducer<Text, IntWritable, Text, Text>{
-        private Text word = new Text();
-        private Text fileInfo = new Text();
-        String filename = new String();
-        static Text currentWord = new Text(" ");
+        static String currentWord =" ";
         static List<String> fileInfoList = new ArrayList<String>();
 
         public void reduce(Text key, Iterable<IntWritable> values, Context context)
                 throws IOException, InterruptedException {
             int sumOfSingleFile = 0;
-            word.set(key.toString().split("#")[0]);
-            filename = key.toString().split("#")[1];
+            String word = key.toString().split("#")[0];
+            String filename = key.toString().split("#")[1];
             for (IntWritable val : values) {
                 sumOfSingleFile += val.get();
             }
-            fileInfo.set(filename + ":" + sumOfSingleFile);
+            Text fileInfo = new  Text(filename + ":" + sumOfSingleFile);
             if (!currentWord.equals(word) && !currentWord.equals(" ")) {
                 StringBuilder out = new StringBuilder();
                 double sumOfFIle = 0, sumOfWord=0;
@@ -89,12 +91,13 @@ public class InvertedIndexer {
                 }
                 fileInfoList = new ArrayList<String>();
             }
-            currentWord = new Text(word);
-            fileInfoList.add(fileInfo.toString()); // 不断向postingList也就是文档名称中添加词表
+            currentWord = word;
+            fileInfoList.add(fileInfo.toString());
         }
 
-        // cleanup 一般情况默认为空，有了cleanup不会遗漏最后一个单词的情况
-
+        /*
+            对最后一个 word 进行输出
+         */
         public void cleanup(Context context) throws IOException,
                 InterruptedException {
             StringBuilder out = new StringBuilder();
@@ -123,7 +126,7 @@ public class InvertedIndexer {
 
         job.setMapperClass(InvertedIndexerMapper.class);
         job.setCombinerClass(SumCombiner.class);
-        job.setPartitionerClass(MyPartitioner.class);
+        job.setPartitionerClass(InvertedIndexerPartitioner.class);
         job.setReducerClass(InvertedIndexerReducer.class);
 
         job.setMapOutputKeyClass(Text.class);
