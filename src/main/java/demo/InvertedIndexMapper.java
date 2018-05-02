bce568652808ea67e9c95d70ae606f28f17876bd
SwimilTylers <swimiltylers@foxmail.com>
2018-05-02 23:29:43 +0800

    Merge branch 'master' into demo-coding-yangyj
    # Conflicts:
    #	src/main/java/InvertedIndexer.java

--- src/main/java/demo/InvertedIndexMapper.java
+++ src/main/java/demo/InvertedIndexMapper.java
@@ -10,6 +10,10 @@ import java.io.IOException;
 import java.util.*;
 
 public class InvertedIndexMapper extends Mapper<LongWritable,Text,Text,IntWritable> {
+    static Set<String> avoid = new HashSet<String>(Arrays.asList(new String[]{
+        "プ", "フ", "ハ", "ネ", "ヌ", "ツ", "チ", "ゼ", "シ", "ザ", "サ", "ゲ", "ケ", "イ",
+            "ィ", "ァ", "は", "ね", "ぬ", "に", "な", "つ", "ち", "そ", "せ", "ず", "し", "ぃ", "ぁ"
+    }));
     @Override
     protected void map(LongWritable key, Text value, Context context)
             throws IOException, InterruptedException
@@ -20,8 +24,10 @@ public class InvertedIndexMapper extends Mapper<LongWritable,Text,Text,IntWritab
         StringTokenizer itr = new StringTokenizer(value.toString());
         while (itr.hasMoreTokens()){
             tmp = itr.nextToken();
-            Text word = new Text(tmp + "#" + fileName);
-            context.write(word, new IntWritable(1));
+            if (!tmp.matches("[0-9a-zA-Z].*") && !avoid.contains(tmp)) {
+                Text word = new Text(tmp + "#" + fileName);
+                context.write(word, new IntWritable(1));
+            }
         }
     }
 }
