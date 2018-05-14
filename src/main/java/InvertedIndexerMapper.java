a9ef4446001a62d8550a244b4a4dbfc33098efc7
SwimilTylers <swimiltylers@foxmail.com>
2018-05-14 23:12:52 +0800

    Partitioner ClassNotFound

--- src/main/java/InvertedIndexerMapper.java
+++ src/main/java/InvertedIndexerMapper.java
@@ -1,3 +1,5 @@
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -6,19 +8,18 @@ import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-public class InvertedIndexerMapper extends Mapper<Object,Text,Text,IntWritable> {
+public class InvertedIndexerMapper extends Mapper<Object,Text,ImmutableBytesWritable,IntWritable> {
     @Override
     public void map(Object key, Text value, Context context) throws IOException,InterruptedException{
         FileSplit fileSplit = (FileSplit) context.getInputSplit();
         String filename = fileSplit.getPath().getName();
         filename=filename.substring(0,filename.length()-14);          // 去除后缀
 
-        Text word = new Text();
         StringTokenizer itr = new StringTokenizer(value.toString());  // 获取文件中所有的单词
 
         while (itr.hasMoreTokens()){
-            word.set(itr.nextToken()+"#"+filename);                   // 使用 # 将word 与 filename 分开
-            context.write(word,new IntWritable(1));                   // 每个单词频度设为1
+            String word = itr.nextToken()+"#"+filename;                   // 使用 # 将word 与 filename 分开
+            context.write(new ImmutableBytesWritable(Bytes.toBytes(word)),new IntWritable(1));                   // 每个单词频度设为1
         }
     }
 }
--- src/main/java/InvertedIndexerReducer.java
+++ src/main/java/InvertedIndexerReducer.java
@@ -1,32 +1,30 @@
 import org.apache.hadoop.hbase.client.Put;
-import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.TableReducer;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
 
 import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-public static class InvertedIndexerReducer extends Reducer<Text, IntWritable, Text, Text> {
-
-    static String currentWord =" ";  // 存储当前reduce方法的word
-    static List<String> fileInfoList = new ArrayList<String>();  // 存储当前word相关的文件信息 格式 文件名:出现次数
+public class InvertedIndexerReducer extends TableReducer<ImmutableBytesWritable, IntWritable, ImmutableBytesWritable> {
+    private MultipleOutputs<Text, Text> multipleOutputs = null;
+    private String currentWord =" ";  // 存储当前reduce方法的word
+    private List<String> fileInfoList = new ArrayList<String>();  // 存储当前word相关的文件信息 格式 文件名:出现次数
 
     public void setup(Context context) throws IOException {
-
+        multipleOutputs = new MultipleOutputs(context);
     }
 
-    public void reduce(Text key, Iterable<IntWritable> values, Context context)
+    public void reduce(ImmutableBytesWritable key, Iterable<IntWritable> values, Context context)
             throws IOException, InterruptedException {
 
         String word = key.toString().split("#")[0];                     // 获取当前key中的word
         String filename = key.toString().split("#")[1];                 // 获取当前key中的filename
-
-        //  get current table
-        Table hTable = connection.getTable(tableName);
 
         // 统计当前单词在当前文件中出现的总次数
         int sumOfSingleFile = 0;
@@ -51,12 +49,13 @@ public static class InvertedIndexerReducer extends Reducer<Text, IntWritable, Te
             DecimalFormat df=new DecimalFormat("#####0.00");     // 格式化词频输出，保留两位小数
             Text wordInfo=new Text(currentWord+"\t"+df.format(sumOfWord/sumOfFIle)+",");
 
-            // raise HTable-put issue
+            // majorOutput: HBase table
             Put put = new Put(Bytes.toBytes(currentWord));
             put.addColumn(Bytes.toBytes("properties"), Bytes.toBytes("frequent"), Bytes.toBytes(sumOfWord/sumOfFIle));
-            hTable.put(put);
+            context.write(new ImmutableBytesWritable(Bytes.toBytes(currentWord)), put);
 
-            context.write(wordInfo, new Text(out.toString()));
+            // multipleOutput: HDFS file
+            multipleOutputs.write(context.getConfiguration().get("HDFSOutputFileName"), new Text(currentWord+", "), new Text(out.toString()), context.getConfiguration().get("HDFSOutputPath"));
 
             // 清空fileInfoList
             fileInfoList = new ArrayList<String>();
@@ -72,9 +71,6 @@ public static class InvertedIndexerReducer extends Reducer<Text, IntWritable, Te
         与reduce中的输出过程一致
      */
     public void cleanup(Context context) throws IOException, InterruptedException {
-        //  get current table
-        Table hTable = connection.getTable(tableName);
-
         StringBuilder out = new StringBuilder();
         double sumOfFIle = 0, sumOfWord=0;
         for (String p : fileInfoList) {
@@ -88,13 +84,15 @@ public static class InvertedIndexerReducer extends Reducer<Text, IntWritable, Te
             DecimalFormat df=new DecimalFormat("#####0.00");
             Text wordInfo=new Text(currentWord+"\t"+df.format(sumOfWord/sumOfFIle)+",");
 
-            // raise HTable-put issue
+            // majorOutput: HBase table
             Put put = new Put(Bytes.toBytes(currentWord));
             put.addColumn(Bytes.toBytes("properties"), Bytes.toBytes("frequent"), Bytes.toBytes(sumOfWord/sumOfFIle));
-            hTable.put(put);
+            context.write(new ImmutableBytesWritable(Bytes.toBytes(currentWord)), put);
 
-            context.write(wordInfo, new Text(out.toString()));
+            // multipleOutput: HDFS file
+            multipleOutputs.write(context.getConfiguration().get("HDFSOutputFileName"), new Text(currentWord+", "), new Text(out.toString()), context.getConfiguration().get("HDFSOutputPath"));
         }
+        multipleOutputs.close();
     }
 
 }
